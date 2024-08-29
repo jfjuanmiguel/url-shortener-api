23725e70f41124dc54f73f65eed11fe81052d126
Jimenez Juan <jimenez.f.juan.m@gmail.com>
2024-08-29 16:06:40 -0400

    chore: Add API key configuration and authentication guard

--- src/modules/url/url.controller.ts
+++ src/modules/url/url.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   Res,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UrlService } from './url.service';
 import { CreateUrlDto } from './dto/create-url.dto';
@@ -16,16 +17,19 @@ import { UrlExistsPipe } from './pipes/url-exists/url-exists.pipe';
 import { Url } from '@prisma/client';
 import { Response } from 'express';
 import { FilterUrlsDto } from './dto/filter-urls.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller()
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
+  @UseGuards(AuthGuard)
   @Post('url')
   create(@Body() createUrlDto: CreateUrlDto) {
     return this.urlService.create(createUrlDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get('url')
   findAll(@Query() queryParams: FilterUrlsDto) {
     return this.urlService.findAll(queryParams);
@@ -36,6 +40,7 @@ export class UrlController {
     res.redirect(url.redirect);
   }
 
+  @UseGuards(AuthGuard)
   @Patch('url/:uid')
   update(
     @Param('uid', UrlExistsPipe) url: Url,
@@ -44,6 +49,7 @@ export class UrlController {
     return this.urlService.update(url.id, updateUrlDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete('url/:uid')
   remove(@Param('uid', UrlExistsPipe) url: Url) {
     return this.urlService.remove(url.id);
